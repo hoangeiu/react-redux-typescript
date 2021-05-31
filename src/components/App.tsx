e8437b17676ee042cb4cc8dec6181861ba6ff93c
hoangeiu <leminhhoangk3@gmail.com>
2021-05-31 08:23:57 +0700

    update files

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RepositoriesList from "./RepositoriesList";
+
+function App() {
+  return (
+    <div>
+      Search For a Package
+      <RepositoriesList />
+    </div>
+  );
+}
+
+export default App;
